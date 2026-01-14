b46194444d6dcc0ea7c087acc4c08ac4d1a8b5e4
jaysphoto <mail@jaysphoto.me>
2026-01-14 17:20:27 +0100

    Added end-to-end tests based on Playwright
    - Adds basic e2e tests for Resume and NotFound pages
    - Github workflow for e2e tests
    - E2E test integration for npm/vitejs with coverage report

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,27 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import IstanbulPlugin from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  build: {
+    sourcemap: true,
+  },
   server: {
     host: "::",
-    port: 8080,
+    port: 5173,
   },
   base: mode === "production" ? "/resume-builder/" : "/",
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+    process.env.USE_BABEL_PLUGIN_ISTANBUL ? [IstanbulPlugin({
+      include: 'src/*',
+      exclude: ['node_modules', 'test/'],
+      extension: [ '.js', '.ts', '.tsx' ],
+    })] : []
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
